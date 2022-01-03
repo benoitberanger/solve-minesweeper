a2b36de7d605430775982eba493ee6af4ff5de03
Benoît Béranger <benoit.beranger@icm-institute.org>
2022-01-03 20:47:59 +0100

    grid location OK + refact

--- test_cv2_rescale.py
+++ test_cv2_rescale.py
@@ -4,26 +4,31 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 import utils
+import logging
 
-convert_grayscle = 1
+logging.basicConfig(format='[%(asctime)s] %(message)s', datefmt='%Y/%d/%m %I:%M:%S', level=logging.INFO)
+logging.info('START')
 
-# all screen image
+convert_grayscale = 1
+
+# screenshot
+logging.info('Screenshot')
 screenshot_all = ImageGrab.grab()
 image_rgb = np.array(screenshot_all)
-
-if convert_grayscle:
+if convert_grayscale:
     image = cv2.cvtColor(image_rgb, cv2.COLOR_BGR2GRAY)
 else:
     image = image_rgb
 
-# load template image
-# template_rgb = cv2.imread('media/t-3.png')
-template_rgb = cv2.imread('media/face0.png')
-if convert_grayscle:
-    template = cv2.cvtColor(template_rgb, cv2.COLOR_BGR2GRAY)
-else:
-    template = template_rgb
-# plt.imshow(template)
-# plt.show()
+# face image recognition
+logging.info('Loading "FACE" image')
+template_face = utils.load_img('media/face0.png', convert_grayscale)
+logging.info('locate_face()')
+face_pos = utils.locate_face(image, template_face)
 
-face_pos = utils.locate_face(image, template)
+# grid location
+logging.info('Loading "TILE" image')
+template_tile = utils.load_img('media/t-3.png', convert_grayscale)
+logging.info('locate_grid()')
+grid_pos = utils.locate_grid(image, template_tile)
+
--- utils.py
+++ utils.py
@@ -1,5 +1,16 @@
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
+
+
+def load_img(path, convert_grayscale):
+    img_rgb = cv2.imread(path)
+    if convert_grayscale:
+        img = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    else:
+        img = img_rgb
+    return img
+
 
 def locate_face(image, template, scales=np.arange(1,10,0.2), threshold_low=0.7, threshold_high=0.90):
 
@@ -29,3 +40,13 @@ def resize_and_match(image, template, factor):
     result = cv2.matchTemplate(image, resized, cv2.TM_CCOEFF_NORMED)
 
     return result
+
+
+def locate_grid(image, template, scales=np.arange(1,10,0.2), threshold=0.9):
+    # small scale test
+    for scale in scales:
+        result = resize_and_match(image, template, scale)
+        grid = np.argwhere(result > threshold)
+        if grid.any():
+            return grid
+
