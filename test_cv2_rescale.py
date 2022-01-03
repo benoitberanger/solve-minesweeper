68e89737b196ded5074eca89e902efe94d074be6
Benoît Béranger <benoit.beranger@icm-institute.org>
2022-01-03 22:50:06 +0100

    added game over face

--- test_cv2_rescale.py
+++ test_cv2_rescale.py
@@ -27,6 +27,9 @@ logging.info('locate_face()')
 face_pos, face_dim = utils.locate_face(image, template_face)
 face_pos = face_pos + face_dim/2  # to be at the center
 
+# reset -> click on th face
+pyautogui.leftClick(face_pos[1], face_pos[0])
+
 # grid location
 logging.info('Loading "TILE" image')
 template_tile = utils.load_img('media/t-3.png', convert_grayscale)
@@ -47,8 +50,10 @@ for i in range(grid_size_h):
     for j in range(grid_size_w):
         tiles_pos_2d[i][j] = (tiles_h_clean[i], tiles_w_clean[j]) + tile_dim/2
 
+# reset -> click on th face
+pyautogui.leftClick(face_pos[1], face_pos[0])
+
 # click in the center tile
 idx_middle_h = round(grid_size_h/2)
 idx_middle_w = round(grid_size_w/2)
-pyautogui.moveTo(tiles_pos_2d[idx_middle_h][idx_middle_w][1], tiles_pos_2d[idx_middle_h][idx_middle_w][0])
-pyautogui.moveTo(face_pos[1], face_pos[0])
+pyautogui.leftClick(tiles_pos_2d[idx_middle_h][idx_middle_w][1], tiles_pos_2d[idx_middle_h][idx_middle_w][0])
